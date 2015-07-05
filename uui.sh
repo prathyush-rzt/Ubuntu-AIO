75594fa64a47fdcede29b95fd2addd3896adae9b
Prathyush <kingspprathyush@gmail.com>
2015-07-05 16:46:47 +0530

    Update uui.sh

--- uui.sh
+++ uui.sh
@@ -3,3 +3,9 @@ wget -q -O - https//dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-ke$
 sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /e$
 sudo apt-get update
 sudo apt-get install google-chrome-stable
+
+#Xampp Installation
+wget http://downloads.sourceforge.net/project/xampp/XAMPP%20Linux/1.8.3/xampp-l$
+sudo chmod +x xampp-linux-x64-1.8.3-4-installer.run
+sudo ./xampp-linux-x64-1.8.3-4-installer.run
+
