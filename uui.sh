5c0873ec8485d3532499b2e9e9ef5a687050d2e3
Prathyush <kingspprathyush@gmail.com>
2015-07-05 16:42:18 +0530

    Create uui.sh

---
+++ uui.sh
@@ -0,0 +1,5 @@
+#Google Chrome 64 bit
+wget -q -O - https//dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-ke$
+sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /e$
+sudo apt-get update
+sudo apt-get install google-chrome-stable
