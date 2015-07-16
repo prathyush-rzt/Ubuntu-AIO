ceaeb2fcb0964e32f8b995022b4ff44fd1dd5e3d
Prathyush <kingspprathyush@gmail.com>
2015-07-17 01:17:11 +0530

    Added Python. Fixed error with Chrome

--- uui.sh
+++ uui.sh
@@ -1,16 +1,33 @@
+
 #Google Chrome 64 bit
-wget -q -O - https//dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-ke$
-sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /e$
+wget -q -O - https//dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
 sudo apt-get update
 sudo apt-get install google-chrome-stable
 
 #Xampp Installation
-wget http://downloads.sourceforge.net/project/xampp/XAMPP%20Linux/1.8.3/xampp-l$
+wget http://downloads.sourceforge.net/project/xampp/XAMPP%20Linux/1.8.3/xampp-linux-x64-1.8.3-4-installer.run
 sudo chmod +x xampp-linux-x64-1.8.3-4-installer.run
 sudo ./xampp-linux-x64-1.8.3-4-installer.run
+
 
 #Git Installation
 sudo apt-get install git
 
 #Graphical SUDO
 sudo apt-get install gksu
+
+#Install Python
+#Install dependecies first
+sudo apt-get install build-essential checkinstall
+sudo apt-get install libreadline-gplv2-dev libncursesw5-dev libssl-dev libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev
+#Download Python
+cd ~/Downloads/
+wget http://python.org/ftp/python/2.7.5/Python-2.7.5.tgz
+#Make Python and Install
+tar -xvf Python-2.7.5.tgz
+cd Python-2.7.5
+./configure
+make
+sudo checkinstall
+
