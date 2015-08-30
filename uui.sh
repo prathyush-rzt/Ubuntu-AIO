6785fcdc741233f1a3ddc7bfffbbe11e8018d459
Prathyush <kingspprathyush@gmail.com>
2015-08-30 17:16:28 +0530

    Update uui.sh

--- uui.sh
+++ uui.sh
@@ -91,7 +91,7 @@ sudo mysql_install_db
 sudo /usr/bin/mysql_secure_installation
 
 #Install PHP5 Engine
-sudo apt-get install php5 libapache2-mod-php5 php5-mcrypt
+sudo apt-get install php5 libapache2-mod-php5 php5-mcrypt php5-xmlrpc
 
 
 
