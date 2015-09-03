3bf624e3083dc6458d7240f064dff7b11c0e9389
Prathyush <kingspprathyush@gmail.com>
2015-09-03 16:16:57 +0530

    Added Caffiene

--- uui.sh
+++ uui.sh
@@ -115,4 +115,8 @@ sudo apt-get install wmctrl
 #Install compizconfig-settings-manager
 sudo apt-get install compizconfig-settings-manager
 
+#Install Caffiene
+sudo apt-add-repository ppa:caffeine-developers/ppa
+sudo apt-get update
+sudo apt-get install caffiene
 
