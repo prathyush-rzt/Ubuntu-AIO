07af8a6a1b28c40cbc516e02b601559af95e792e
Prathyush <kingspprathyush@gmail.com>
2015-11-24 12:24:10 +0530

    Added MongoDB

--- uui.sh
+++ uui.sh
@@ -183,3 +183,9 @@ sudo apt-get install sbt
 
 #Install Aria2 Download Manager (Better than wget)
 sudo apt-get install aria2
+
+#Mongo-DB
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+echo "deb http://repo.mongodb.org/apt/ubuntu "$(lsb_release -sc)"/mongodb-org/3.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.0.list
+sudo apt-get update
+sudo apt-get install -y mongodb-org
